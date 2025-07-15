14378c62b0a169eda85a9f4808bd0fb22e1d1fd4
Valentin Vasilev <valio_vv@abv.bg>
2025-07-15 20:57:47 +0300

    Full Project - January 2024

---
+++ File System/FileSystem.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using static File_System.RunnerProgram;
+
+namespace File_System
+{
+
+    class FileSystem
+    {
+        private FileStream containerFile;
+        private readonly int baseDirOffset;
+        internal DataBlock currentDirectory;
+        private readonly BinaryWriter _bw;
+        private readonly BinaryReader _br;
+
+        public FileSystem(FileStream fileStream)
+        {
+            string basedirName = "base";
+
+            containerFile = fileStream;
+            _bw = new BinaryWriter(containerFile, Encoding.ASCII, true);
+            _br = new BinaryReader(containerFile, Encoding.ASCII, true);
+            currentDirectory = new DataBlock(basedirName, 0, DataBlockType.Directory, -1);
+            baseDirOffset = 24;
+
+            _bw.Write(currentDirectory.ToString());
+
+        }
+
+        public enum DataBlockType
+        {
+            Directory,
+            File
+        }
+
+        public class DataBlock
+        {
+            public string Path { get; set; } = "";
+            public long Position { get; set; }
+            public long Size { get; set; }
+            public DataBlockType Type { get; set; }
+            public long Parent { get; set; }
+            public string Content { get; set; }
+
+            public DataBlock(string path, long position, DataBlockType type, long parent, string content = null)
+            {
+                Path = path;
+                Position = position;
+                Type = type;
+                Parent = parent;
+                Content = content;
+                Size = this.ToString().Length;
+
+            }
+
+            public override string ToString()
+            {
+                string dataBlockString;
+
+                dataBlockString = Path + ',' + Position + ',' + Size + ',' + Type + ',' + Parent + ',' + Content;
+
+
+                return dataBlockString;
+            }
+
+
+        }
+
+        private void CreateDataBlock(string directoryPath, DataBlockType type, long parent, string content = null)
+        {
+            DataBlock newDB = new DataBlock(directoryPath, containerFile.Length, type, parent);
+            containerFile.Position = containerFile.Length;
+
+            if (newDB.Type == DataBlockType.File)
+                newDB.Content = content;
+            else
+                ChangeCurrentDirectory(newDB);
+
+            _bw.Write(newDB.ToString());
+
+
+
+
+        }
+
+        public bool CheckIfDirectoryExists(string directoryName)
+        {
+
+            containerFile.Position = 0;
+
+            while (_br.PeekChar() != -1)
+            {
+                string currentDataBlock = _br.ReadString();
+
+                if (CustomContains(currentDataBlock, directoryName) && CustomContains(currentDataBlock, "Directory"))
+                    return true;
+
+
+            }
+            return false;
+
+        }
+
+
+        private long FindDataBlockPositionByName(string directoryName, DataBlockType type)
+        {
+
+            if (directoryName == "base")
+                return 0;
+
+
+            long position = -1;
+            string[] directories = CustomSplit(directoryName, '\\');
+            string searched = directories[directories.Length - 1];
+
+            containerFile.Position = baseDirOffset;
+
+            while (_br.PeekChar() != -1)
+            {
+                string currentDataBlock = _br.ReadString();
+
+                if (CustomContains(currentDataBlock, searched) && CustomContains(currentDataBlock, type.ToString()))
+                {
+                    position = (long)Convert.ToDouble(CustomSplit(currentDataBlock, ',')[1]);
+                    break;
+                }
+
+            }
+
+            return position;
+        }
+
+        private void ChangeCurrentDirectory(DataBlock dir)
+        {
+
+            currentDirectory = dir;
+
+        }
+
+        public void Mkdir(string directoryName)
+        {
+
+            if (CheckIfDirectoryExists(directoryName))
+            {
+                Console.WriteLine("Directory with that name already exists!");
+            }
+            else
+            {
+
+                string newDirectoryPath = CustomPathCombine(currentDirectory.Path, directoryName);
+                CreateDataBlock(newDirectoryPath, DataBlockType.Directory, FindDataBlockPositionByName(currentDirectory.Path, DataBlockType.Directory));
+
+            }
+
+        }
+
+
+        public void Cd(string directoryName)
+        {
+            long position = FindDataBlockPositionByName(directoryName, DataBlockType.Directory);
+            if (position == -1)
+                Console.WriteLine("A directory with that name wasn't found!");
+            else
+            {
+                containerFile.Position = position;
+                currentDirectory = StringToDataBlock(_br.ReadString(), DataBlockType.Directory);
+
+            }
+
+        }
+
+        private DataBlock StringToDataBlock(string dataBlockInfo, DataBlockType type)
+        {
+            DataBlock savedDataBlock;
+
+            string[] data = CustomSplit(dataBlockInfo, ',');
+
+            savedDataBlock = new DataBlock(data[0], (long)Convert.ToDouble(data[1]), type, (long)Convert.ToDouble(data[4]));
+
+            if (type == DataBlockType.File)
+                savedDataBlock.Content = data[5];
+
+
+            return savedDataBlock;
+        }
+
+        public void Write(string fileName, string content = null)
+        {
+
+
+            if (CheckIfFileExists(fileName) != null)
+            {
+                //Edit existing file
+                Console.WriteLine("A file with that name already exists in this directory!");
+
+            }
+            else
+            {
+                //Create new file
+                string newDirectoryPath = CustomPathCombine(currentDirectory.Path, fileName);
+                CreateDataBlock(newDirectoryPath, DataBlockType.File, FindDataBlockPositionByName(currentDirectory.Path, DataBlockType.Directory), content);
+
+
+            }
+
+        }
+
+
+        private DataBlock CheckIfFileExists(string fileName, bool everywhere = false)
+        {
+
+            containerFile.Position = 0;
+
+            long newFileParent = FindDataBlockPositionByName(CustomSplit(currentDirectory.Path, '\\')[CustomSplit(currentDirectory.Path, '\\').Length - 1], DataBlockType.Directory);
+
+            while (_br.PeekChar() != -1)
+            {
+                string currentDataBlock = _br.ReadString();
+                long currentParent = (long)Convert.ToDouble(CustomSplit(currentDataBlock, ',')[4]);
+
+                if (everywhere)
+                {
+                    if (CustomContains(currentDataBlock, fileName) && CustomContains(currentDataBlock, "File"))
+                        return StringToDataBlock(currentDataBlock, DataBlockType.File);
+                }
+                else
+                {
+                    if (CustomContains(currentDataBlock, fileName) && currentParent == newFileParent && CustomContains(currentDataBlock, "File"))
+                        return StringToDataBlock(currentDataBlock, DataBlockType.File);
+                }
+
+            }
+
+            return null;
+
+        }
+
+        public void Ls(string directoryName)
+        {
+
+            containerFile.Position = 0;
+
+            long searchedPosition = FindDataBlockPositionByName(directoryName, DataBlockType.Directory);
+
+            containerFile.Position = 0;
+
+            while (_br.PeekChar() != -1)
+            {
+                string currentDataBlock = _br.ReadString();
+                string[] data = CustomSplit(currentDataBlock, ',');
+
+                if (searchedPosition != -1)
+                {
+                    if ((long)Convert.ToDouble(data[4]) == searchedPosition)
+                        if (data[3].Equals("Directory"))
+                            Console.WriteLine(CustomSplit(data[0], '\\')[CustomSplit(data[0], '\\').Length - 1] + '\\');
+                        else
+                            Console.WriteLine(CustomSplit(data[0], '\\')[CustomSplit(data[0], '\\').Length - 1]);
+
+
+                }
+            }
+
+        }
+
+        public void Cat(string fileName)
+        {
+
+            long searched = FindDataBlockPositionByName(fileName, DataBlockType.File);
+
+            containerFile.Position = searched;
+
+            DataBlock db = StringToDataBlock(_br.ReadString(), DataBlockType.File);
+
+            Console.WriteLine(db.Content);
+
+        }
+
+        public void Cp(string fileName, string directoryName)
+        {
+            if (CheckIfDirectoryExists(directoryName) && CheckIfFileExists(fileName, true) != null)
+            {
+
+                long searchedFile = FindDataBlockPositionByName(fileName, DataBlockType.File);
+                long searchedDirectory = FindDataBlockPositionByName(directoryName, DataBlockType.Directory);
+
+
+                containerFile.Position = searchedDirectory;
+                DataBlock newDir = StringToDataBlock(_br.ReadString(), DataBlockType.Directory);
+
+                ChangeCurrentDirectory(newDir);
+
+                if (CheckIfFileExists(fileName, false) == null)
+                {
+
+                    containerFile.Position = searchedFile;
+
+                    string[] data = CustomSplit(_br.ReadString(), ',');
+
+                    string fileContent = data[data.Length - 1];
+
+
+                    string newFilePath = CustomPathCombine(newDir.Path, fileName);
+                    DataBlock newFile = new DataBlock(newFilePath, containerFile.Length, DataBlockType.File, searchedDirectory, fileContent);
+
+
+                    containerFile.Position = containerFile.Length;
+                    _bw.Write(newFile.ToString());
+                }
+                else
+                    Console.WriteLine("A file with that name already exists there!");
+
+            }
+            else
+                Console.WriteLine("The File or Directory specified not found!");
+        }
+
+        public void Import(string sourcePath, string destinationDirectoryName, string content = null)
+        {
+            if (CheckIfDirectoryExists(destinationDirectoryName) && File.Exists(sourcePath))
+            {
+
+                string fileContent = File.ReadAllText(sourcePath);
+
+                if (content != null)
+                    fileContent += content;
+
+                sourcePath = CustomTrimEnd(sourcePath, '\\');
+
+                string fileName = CustomSplit(sourcePath, '\\')[CustomSplit(sourcePath, '\\').Length - 1];
+
+
+                long searchedDirectory = FindDataBlockPositionByName(destinationDirectoryName, DataBlockType.Directory);
+
+
+                containerFile.Position = searchedDirectory;
+                DataBlock newDir = StringToDataBlock(_br.ReadString(), DataBlockType.Directory);
+
+                ChangeCurrentDirectory(newDir);
+
+                if (CheckIfFileExists(fileName, false) == null)
+                {
+
+
+                    string newFilePath = CustomPathCombine(newDir.Path, fileName);
+                    DataBlock newFile = new DataBlock(newFilePath, containerFile.Length, DataBlockType.File, searchedDirectory, fileContent);
+
+
+                    containerFile.Position = containerFile.Length;
+                    _bw.Write(newFile.ToString());
+                }
+                else
+                    Console.WriteLine("A file with that name already exists there!");
+
+            }
+            else
+                Console.WriteLine("The File or Directory specified not found!");
+        }
+
+        public void Export(string fileName, string destinationDirectoryPath)
+        {
+            if (CheckIfFileExists(fileName, true) != null)
+            {
+
+                long searchedFile = FindDataBlockPositionByName(fileName, DataBlockType.File);
+
+                containerFile.Position = searchedFile;
+
+                string[] data = CustomSplit(_br.ReadString(), ',');
+
+
+                string newFilePath = CustomPathCombine(destinationDirectoryPath, CustomSplit(data[0], '\\')[CustomSplit(data[0], '\\').Length - 1]);
+
+                File.WriteAllText(newFilePath, data[data.Length - 1]);
+
+            }
+            else
+                Console.WriteLine("File or destination Directory not found!");
+        }
+
+    }
+}
+
---
+++ File System/RunnerProgram.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.IO;
+
+namespace File_System
+{
+    class RunnerProgram
+    {
+
+        public static string CustomPathCombine(string path1, string path2)
+        {
+            if (path2[0] == '\\')
+                return path1 + path2;
+
+            else
+                return path1 + '\\' + path2;
+        }
+
+        public static bool CustomContains(string source, string subString)
+        {
+            for (int i = 0; i <= source.Length - subString.Length; i++)
+            {
+                bool match = true;
+
+                for (int j = 0; j < subString.Length; j++)
+                {
+                    if (source[i + j] != subString[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                    return true;
+            }
+
+            return false;
+        }
+
+        internal static string[] CustomSplit(string input, char delimiter)
+        {
+            int count = CountOccurrences(input, delimiter) + 1;
+            string[] result = new string[count];
+
+            int startIndex = 0;
+            int resultIndex = 0;
+
+            for (int i = 0; i < input.Length; i++)
+                if (input[i] == delimiter)
+                {
+                    result[resultIndex++] = input.Substring(startIndex, i - startIndex);
+                    startIndex = i + 1;
+                }
+
+
+            result[resultIndex] = GetStringSlice(input, startIndex);
+
+            return result;
+        }
+
+        internal static string GetStringSlice(string input, int startIndex, int length = -1)
+        {
+            if (length == -1)
+            {
+                length = input.Length - startIndex;
+            }
+
+            char[] result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = input[startIndex + i];
+            }
+
+            return new string(result);
+        }
+
+        internal static int CountOccurrences(string input, char target)
+        {
+            int count = 0;
+
+            foreach (char c in input)
+                if (c == target)
+                    count++;
+
+            return count;
+        }
+
+        public static string CustomTrim(string input)
+        {
+            int startIndex = 0;
+            int endIndex = input.Length - 1;
+
+            while (startIndex <= endIndex && input[startIndex] == ' ')
+                startIndex++;
+
+
+            while (endIndex >= startIndex && input[endIndex] == ' ')
+                endIndex--;
+
+
+            if (startIndex > endIndex)
+                return string.Empty;
+
+            else
+            {
+                int length = endIndex - startIndex + 1;
+                char[] trimmedArray = new char[length];
+
+                for (int i = 0; i < length; i++)
+                    trimmedArray[i] = input[startIndex + i];
+
+                return new string(trimmedArray);
+            }
+        }
+
+        internal static string StringToBinary(string input)
+        {
+            char[] binaryResult = new char[input.Length * 8];
+
+            int index = 0;
+
+            foreach (char c in input)
+                for (int i = 7; i >= 0; i--)
+                    binaryResult[index++] = ((c & (1 << i)) != 0) ? '1' : '0';
+
+            return new string(binaryResult, 0, index);
+        }
+
+        internal static string BinaryToString(string binaryInput)
+        {
+            int length = binaryInput.Length;
+            char[] result = new char[length / 8];
+
+            for (int i = 0; i < length; i += 8)
+            {
+                int decimalValue = 0;
+
+                for (int j = 0; j < 8; j++)
+                {
+                    int binaryDigitIndex = i + j;
+                    if (binaryDigitIndex < length && binaryInput[binaryDigitIndex] == '1')
+                    {
+                        decimalValue += (1 << (7 - j));
+                    }
+                }
+
+                result[i / 8] = (char)decimalValue;
+            }
+
+            return new string(result);
+        }
+
+        public static string CustomTrimEnd(string input, char trimChar)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            int length = input.Length;
+            int trimLength = 0;
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                if (input[i] != trimChar)
+                {
+                    break;
+                }
+
+                trimLength++;
+            }
+
+            return trimLength == 0 ? input : input.Substring(0, length - trimLength);
+        }
+
+        static void Main(string[] args)
+        {
+
+            string pathName = @".\fileSystem.txt";
+
+            FileStream fs = new FileStream(pathName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            FileSystem fileSystemSimulator = new FileSystem(fs);
+
+            while (true)
+            {
+                Console.WriteLine($"\nCurrent dir: {fileSystemSimulator.currentDirectory.Path}\\");
+                Console.Write("Enter command: ");
+                string userInput = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(userInput))
+                    continue; // Skip empty input
+                
+
+                string[] commandArgs = CustomSplit(userInput, ' ');
+                string command = commandArgs[0].ToLower();
+
+                switch (command)
+                {
+                    case "mkdir":
+
+                        if (commandArgs.Length == 2)
+                            fileSystemSimulator.Mkdir(commandArgs[1]);
+
+                        else
+                            Console.WriteLine("Usage: mkdir <directory_name>");
+
+                        break;
+
+                    case "cd":
+
+                        if (commandArgs.Length == 2)
+                            fileSystemSimulator.Cd(commandArgs[1]);
+
+                        else
+                            Console.WriteLine("Usage: cd <directory_name>");
+
+                        break;
+
+                    case "write":
+
+                        if (commandArgs.Length == 2)
+                            fileSystemSimulator.Write(commandArgs[1]);
+
+                        else if (commandArgs.Length >= 4)
+                        {
+                            string textContent = CustomSplit(userInput, '\"')[1];
+                            fileSystemSimulator.Write(commandArgs[1], textContent);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Usage: write <file_name>");
+                            Console.WriteLine("or");
+                            Console.WriteLine("Usage: write <file_name> +append \"<file_content>\"");
+                        }
+                        break;
+
+                    case "ls":
+
+                        if (commandArgs.Length == 2)
+                            fileSystemSimulator.Ls(commandArgs[1]);
+
+                        else
+                            Console.WriteLine("Usage: ls <directory_name>");
+
+                        break;
+
+                    case "cat":
+
+                        if (commandArgs.Length == 2)
+                            fileSystemSimulator.Cat(commandArgs[1]);
+
+                        else
+                            Console.WriteLine("Usage: cat <file_name>");
+
+                        break;
+
+                    case "cp":
+
+                        if (commandArgs.Length == 3)
+                            fileSystemSimulator.Cp(commandArgs[1], commandArgs[2]);
+
+                        else
+                            Console.WriteLine("Usage: cp <file_name> <directory_name>");
+
+                        break;
+
+                    case "import":
+
+                        if (commandArgs.Length >= 3)
+                        {
+
+                            string[] commands = CustomSplit(userInput, '\"');
+
+                            if (!userInput.Contains("+append"))
+                                fileSystemSimulator.Import(commands[1], CustomTrim(commandArgs[2]));
+
+                            else
+                                fileSystemSimulator.Import(commands[1], CustomTrim(commandArgs[2]), commands[3]);
+
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Usage: import \"<file_path>\" <directory_name>");
+                            Console.WriteLine("or");
+                            Console.WriteLine("Usage: import \"<file_path>\" <directory_name> +append \"<extra_file_content>\"");
+                        }
+
+                        break;
+
+                    case "export":
+
+                        if (commandArgs.Length >= 3)
+                        {
+
+                            string[] commands = CustomSplit(userInput, '\"');
+                            fileSystemSimulator.Export(commandArgs[1], commands[1]);
+
+                        }
+
+                        else
+                            Console.WriteLine("Usage: export <file_name> \"<directory_path>\"");
+
+
+                        break;
+
+                    case "exit":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid command.");
+                        break;
+                }
+            }
+
+        }
+
+    }
+
+
+
+}
